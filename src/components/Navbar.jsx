80acd470820ee8a204c148056a451b9c36270ad0
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-12-24 23:51:59 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Navbar.css"
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                    <li>Item 5</li>
+                    <li>Item 6</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import "./Home.css"
+const Home = () => {
+  return (
+    <div className='box'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
